baef4bf1878985c22264210f657e33359747766f
bembeev <47711661+bembeev@users.noreply.github.com>
2023-06-06 21:59:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.firstname}
-${tg.initDataUnsafe.user.lastname}`
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`
 
 usercard.appendChild(p);
  
